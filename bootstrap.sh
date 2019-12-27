04093611ccfc1d94681953576e39a993b989f448
Dean Herbert <pe@ppy.sh>
2019-12-27 15:47:41 +0900

    Simpler

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,13 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+git pull;
 
 function doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
+		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
 }
