111a81002d583f590882cf868d982ab1414826df
Dean Herbert <pe@ppy.sh>
2019-12-27 15:36:10 +0900

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# taken from https://github.com/mathiasbynens/dotfiles
+
+cd "$(dirname "${BASH_SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude "bootstrap.sh" \
+		-avh --no-perms . ~;
+	source ~/.bash_profile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
