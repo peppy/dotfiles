1df735f5701cf257d7f2201be1511ab912c0b4b5
Dean Herbert <pe@ppy.sh>
2023-08-06 15:18:29 +0200

    Improve before-after command to support more than one comparison

--- scripts/executable_ghfm-before-after.sh
+++ scripts/executable_ghfm-before-after.sh
@@ -3,10 +3,10 @@
 # Required parameters:
 # @raycast.schemaVersion 1
 # @raycast.title GHFM before-after
-# @raycast.mode inline
+# @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 🤖
+# @raycast.icon 📊
 
 # Documentation:
 # @raycast.author ppy
@@ -15,10 +15,19 @@
 # Read the clipboard and split on newlines
 IFS=$'\n' read -d '' -r -a images < <(pbpaste)
 
-# Generate the GHFM table and copy to clipboard
+# Start with the header
 result="| Before | After |
-| :---: | :---: |
-| ${images[0]} | ${images[1]} |"
+| :---: | :---: |"
+
+# Loop over the array two items at a time
+for ((i=0; i<${#images[@]}; i+=2))
+do
+  # Add a row for each pair of images
+  result="$result
+| ${images[$i]} | ${images[$i+1]} |"
+done
+
+# Display the result inline
 echo -n "$result" | pbcopy
 
-echo -n "Copied to clipboard!"
+echo -n "Copied output to clipboard!"
