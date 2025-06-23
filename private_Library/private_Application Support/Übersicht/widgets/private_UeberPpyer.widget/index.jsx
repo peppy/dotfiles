b6b28b7d0a85bcf3edf8c7eb35c87693aa418c8d
Dean Herbert <pe@ppy.sh>
2025-06-23 16:44:45 +0900

    Update ubersicht now playing to use `mediaremote`

--- private_Library/private_Application Support/Übersicht/widgets/private_UeberPpyer.widget/index.jsx
+++ private_Library/private_Application Support/Übersicht/widgets/private_UeberPpyer.widget/index.jsx
@@ -4,30 +4,26 @@
    Using applescript causes system-wide GPU stutters when interacting with apple music for whatever
    fucked up reason.
 
-   To use this, you will need to run my fork of `nowplaying-cli` as a background service
-   https://github.com/peppy/nowplaying-cli
+   This now uses (and depends on) https://github.com/ungive/mediaremote-adapter.
+   As of macOS 15.5, this is one remaining method of getting system-wide now-playing information.
 
-   The binary is included alongside this widget for convenience.
-
-   Note that this whole implementation using the system now playing API.
-   This means it is not limited to apple music. It also work with airplay.
+   Install it via brew.
  */
 
 import { styled, run } from "uebersicht";
 import getColors from './lib/getColors.js';
-const _version = '1.4.0';
 
 export const refreshFrequency = 1000;
 
 const options = {
-  verticalPosition: "bottom",      // -> top (default) | center | bottom | "<number>" | "-<number>"
-  horizontalPosition: "right",   // -> left (default) | center | right | "<number>" | "-<number>"
-  alwaysShow: 0,                // -> 0 (default) | 1 | 2
-  adaptiveColors: true,         // -> true (default) | false
-  minContrast: 2.6,             // -> 2.6 (default) | number
-  dualProgressBar: false,       // -> true | false (default)
-  cacheMaxDays: 15,             // -> 15 (default) | <number>
-}
+  verticalPosition: "bottom",
+  horizontalPosition: "right",
+  alwaysShow: 0,
+  adaptiveColors: true,
+  minContrast: 2.6,
+  dualProgressBar: false,
+  cacheMaxDays: 15,
+};
 
 export const className = `
   pointer-events: none;
@@ -38,7 +34,6 @@ export const className = `
   right: 0;
   font-family: "Jetbrains Mono", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   color: white;
-
   * {
     box-sizing: border-box;
     padding: 0;
@@ -47,44 +42,57 @@ export const className = `
   }
 `;
 
-/* EMOTION COMPONENTS */
-
 const wrapperPos = ({ horizontal, vertical }) => {
   if (horizontal === "center" && vertical === "center") {
     return `
       top: 50%;
       left: 50%;
       transform: translate(-50%, -50%);
-    `
+    `;
   }
-
   let hPos, vPos;
   switch (horizontal) {
-    case "left": hPos = `left: 20px;`; break;
-    case "center": hPos = `left: 50%; transform: translateX(-50%);`; break;
-    case "right": hPos = `right: 8px;`; break;
-    default: hPos = horizontal.startsWith("-") ? `right: ${parseInt(horizontal) * -1}px;` : `left: ${horizontal}px;`; break;
+    case "left":
+      hPos = `left: 20px;`;
+      break;
+    case "center":
+      hPos = `left: 50%; transform: translateX(-50%);`;
+      break;
+    case "right":
+      hPos = `right: 8px;`;
+      break;
+    default:
+      hPos = horizontal.startsWith("-")
+        ? `right: ${parseInt(horizontal) * -1}px;`
+        : `left: ${horizontal}px;`;
   }
   switch (vertical) {
-    case "top": vPos = `top: 20px;`; break;
-    case "center": vPos = `top: 50%; transform: translateY(-50%);`; break;
-    case "bottom": vPos = `bottom: 8px;`; break;
-    default: vPos = vertical.startsWith("-") ? `bottom: ${parseInt(vertical) * -1}px;` : `top: ${vertical}px;`; break;
+    case "top":
+      vPos = `top: 20px;`;
+      break;
+    case "center":
+      vPos = `top: 50%; transform: translateY(-50%);`;
+      break;
+    case "bottom":
+      vPos = `bottom: 8px;`;
+      break;
+    default:
+      vPos = vertical.startsWith("-")
+        ? `bottom: ${parseInt(vertical) * -1}px;`
+        : `top: ${vertical}px;`;
   }
-
   return `${hPos} ${vPos}`;
-}
+};
 
 const Wrapper = styled("div")`
   position: absolute;
   overflow: hidden;
   box-shadow: 0 14px 30px 5px #0005;
   border-radius: 14px;
-  opacity: ${props => props.show ? 1 : 0};
-  background: ${props => (props.bg !== undefined) ? props.bg : "#00040000"};
+  opacity: ${props => (props.show ? 1 : 0)};
+  background: ${props => (props.bg !== undefined ? props.bg : "#00040000")};
   transition: all 0.6s cubic-bezier(0.22, 1, 0.36, 1);
   ${wrapperPos}
-
   &::before {
     content: "";
     position: absolute;
@@ -94,7 +102,6 @@ const Wrapper = styled("div")`
     right: 0;
     z-index: -1;
   }
-
 `;
 
 const SmallPlayer = styled("div")`
@@ -102,18 +109,16 @@ const SmallPlayer = styled("div")`
   display: flex;
   height: 50px;
   width: 400px;
-`
+`;
 
 const ArtworkWrapper = styled("div")`
   position: relative;
   width: 240px;
   height: 240px;
-
   &.small {
     width: 80px;
     height: 80px;
   }
-
   &::before {
     position: absolute;
     content: "";
@@ -125,11 +130,10 @@ const ArtworkWrapper = styled("div")`
     background: #fff1;
     z-index: -1;
   }
-
   &.mini::before {
     border-radius: 6px;
   }
-`
+`;
 
 const Artwork = styled("img")`
   position: absolute;
@@ -137,8 +141,8 @@ const Artwork = styled("img")`
   width: 100%;
   overflow: hidden;
   object-fit: cover;
-  border-radius: 0px;
-  opacity: ${props => props.show ? 1 : 0};
+  border-radius: 0;
+  opacity: ${props => (props.show ? 1 : 0)};
 `;
 
 const Information = styled("div")`
@@ -146,14 +150,12 @@ const Information = styled("div")`
   padding: .5em .75em;
   line-height: 1.3;
   border-radius: 14px;
-
   > p {
     text-align: center;
     white-space: nowrap;
     overflow: visible;
     text-overflow: ellipsis;
   }
-
   &.small {
     flex: 1;
     width: 0;
@@ -162,11 +164,10 @@ const Information = styled("div")`
     justify-content: center;
     line-height: 1.2;
   }
-
   &.small > p {
     text-align: left;
   }
-`
+`;
 
 const Progress = styled("div")`
   position: absolute;
@@ -175,7 +176,6 @@ const Progress = styled("div")`
   right: 0;
   height: 2px;
   background: ${props => options.dualProgressBar && props.emptyColor ? (props.emptyColor + "80") : "transparent"};
-
   &::after {
     content: "";
     position: absolute;
@@ -186,7 +186,6 @@ const Progress = styled("div")`
     background: ${props => props.progressColor ? props.progressColor : "white"};
     transition: width .6s cubic-bezier(0.22, 1, 0.36, 1);
   }
-
   &.small {
     top: initial;
     bottom: 0;
@@ -200,102 +199,90 @@ const Progress = styled("div")`
     overflow: hidden;
     margin-top: 10px;
   }
-`
+`;
 
 const Track = styled("p")`
   font-weight: bold;
   font-size: .7em;
   color: ${props => props.color ? props.color : "inherit"};
-
   &::after {
     content: '';
     display: inline-block;
     width: 0;
   }
-
   &.small {
     font-size: .65em;
   }
-
   &.mini {
     font-size: 1.2em;
   }
-`
+`;
 
 const Artist = styled("p")`
   font-size: .7em;
   color: ${props => props.color ? props.color : "inherit"};
-
   &::after {
     content: '';
     display: inline-block;
     width: 0;
   }
-
   &.small {
     font-size: .65em;
   }
-
   &.mini {
     font-size: 1em;
   }
-`
+`;
 
 const Album = styled("p")`
   font-size: .65em;
   color: ${props => props.color ? props.color : "inherit"};
   opacity: .75;
-
   &::after {
     content: '';
     display: inline-block;
     width: 0;
   }
-
   &.small {
     font-size: .55em;
   }
-`
+`;
 
-/* UEBER-SPECIFIC STUFF */
-
-export const init = (dispatch) => {
-  // Initialize and clear cache of old artwork
+export const init = dispatch => {
   run(
     `mkdir -p UeberPpyer.widget/cache &&\
     find UeberPpyer.widget/cache -mindepth 1 -type f -mtime +${options.cacheMaxDays} -delete`
   );
 };
 
-export const command = "cat UeberPpyer.widget/lib/track";
+export const command = "/opt/homebrew/bin/media-control get";
 
 export const initialState = {
-  app: "",                                          // Current music software being used
-  playing: false,                                   // If currently playing a soundtrack
-  appleMusicError: false,                           // If online music is being played on Apple Music
-  songChange: false,                                // If the song changed
-  primaryColor: undefined,                          // Primary color from artwork
-  secondaryColor: undefined,                        // Secondary color from artwork
-  tercaryColor: undefined,                          // Tercary color from artwork
-  artwork: {                                        // Artwork source URL to be used
-    art1: "UeberPpyer.widget/default.png",           // Artwork to alternate with
-    art2: "UeberPpyer.widget/default.png",           // Same as above
-    alternate: true                                   // Flag to pick which artwork to display (for smooth transitions)
+  app: "",
+  playing: false,
+  appleMusicError: false,
+  songChange: false,
+  primaryColor: undefined,
+  secondaryColor: undefined,
+  tercaryColor: undefined,
+  artwork: {
+    art1: "UeberPpyer.widget/default.png",
+    art2: "UeberPpyer.widget/default.png",
+    alternate: true
   },
-  song: {                                           // Current song data
-    track: "",                                        // Name of soundtrack
-    artist: "",                                       // Name of artist
-    album: "",                                        // Name of album
-    localArtwork: "",                                 // Locally stored url for album artwork
-    onlineArtwork: "",                                // Online url for album artwork
-    duration: 0,                                      // Total duration of soundtrack in seconds
-    elapsed: 0                                        // Total time elapsed in seconds
+  song: {
+    track: "",
+    artist: "",
+    album: "",
+    localArtwork: "",
+    onlineArtwork: "",
+    duration: 0,
+    elapsed: 0
   },
-  updateAvailable: false,                           // Flag for when an update's available
-  updatePending: false,                             // Flag for when an update's about to happen
+  updateAvailable: false,
+  updatePending: false,
 };
 
-// Update state
 export const updateState = ({ type, output, error }, previousState) => {
   switch (type) {
     case 'UB/COMMAND_RAN':
@@ -336,40 +323,41 @@ export const updateState = ({ type, output, error }, previousState) => {
       console.error("Invalid dispatch type?");
       return previousState;
   }
-}
+};
 
-/* FUNCTIONS */
-
-// Update song metadata
 const updateSongData = (output, error, previousState) => {
-  // Check for errors
   if (error) {
-    const err_output = { ...previousState, error: error };
+    const err_output = { ...previousState, error };
     console.error(err_output);
     return err_output;
   }
 
-  // Extract & parse applescript output
-  let [
-    playing,
-    app,
-    track,
-    artist,
-    album,
-    artworkURL,
-    artworkFilename,
-    duration,
-    elapsed,
-    appleMusicError
-  ] = output.trim().split("\n");
+  const data = JSON.parse(output);
 
-  playing = (playing === "true");
-  appleMusicError = false;
-  duration = Math.floor(parseFloat(duration));
-  elapsed = Math.floor(parseFloat(elapsed));
+  if (!data)
+    return previousState;
 
-  // State controller
-  if (track !== previousState.song.track || album !== previousState.song.album) {    // Song change
+  const playing = data.playing;
+  const app = data.bundleIdentifier;
+  const track = data.title;
+  const artist = data.artist;
+  const album = data.album;
+  const appleMusicError = false;
+  const duration = data.duration;
+  const timeDiff = Date.now() - new Date(data.timestamp).getTime();
+  let elapsed = data.elapsedTime;
+  if (data.playing) {
+    elapsed += timeDiff / 1000;
+  }
+  let localArtwork = null;
+  if (data.artworkMimeType) {
+    localArtwork = `data:${data.artworkMimeType};base64,${data.artworkData}`;
+  }
+  if (
+    (localArtwork && !previousState?.song.localArtwork) ||
+    track !== previousState?.song.track ||
+    album !== previousState?.song.album
+  ) {
     return {
       ...previousState,
       app,
@@ -380,13 +368,12 @@ const updateSongData = (output, error, previousState) => {
         track,
         artist,
         album,
-        localArtwork: `UeberPpyer.widget/cache/${artworkFilename}`,
-        onlineArtwork: artworkURL,
+        localArtwork,
         duration,
         elapsed
       }
-    }
-  } else {  // Currently playing
+    };
+  } else {
     return {
       ...previousState,
       app,
@@ -398,78 +385,57 @@ const updateSongData = (output, error, previousState) => {
       }
     };
   }
-}
+};
 
-// Prepare artwork
 const prepareArtwork = (dispatch, song) => {
-  // Use a dummy image to test images beforehand
-  const img = new Image();
-
-  // Attempts images in this order: Local -> Online -> Default
-  img.onload = () => { dispatch({ type: "GET_ART", output: { img }})};
+  const img = new window.Image();
+  img.onload = () => {
+    dispatch({ type: "GET_ART", output: { img } });
+  };
   img.onerror = () => {
-    if (song.onlineArtwork !== "missing value" && song.onlineArtwork !== "" && img.src !== song.onlineArtwork) {
-      // img.crossOrigin = 'anonymous';
-      // img.src = song.onlineArtwork;
-    } else {
-      dispatch({ type: "DEFAULT_ART" });
-    }
-  }
-
+    dispatch({ type: "DEFAULT_ART" });
+  };
   img.crossOrigin = 'anonymous';
   img.src = song.localArtwork;
-}
+};
 
-// RENDERING //
-// Artwork image
 const ArtworkImage = ({ artwork: { art1, art2, alternate }, wrapperClass }) => (
   <ArtworkWrapper className={wrapperClass}>
-    <Artwork src={art1} show={alternate}/>
-    <Artwork src={art2} show={!alternate}/>
+    <Artwork src={art1} show={alternate} />
+    <Artwork src={art2} show={!alternate} />
   </ArtworkWrapper>
-)
+);
 
-// Small player component
-const Small = ({ state, dispatch }) => {
+const Small = ({ state }) => {
   const {
     song: { track, artist, album, elapsed, duration },
     secondaryColor,
     tercaryColor,
     artwork,
-    updateAvailable,
-    updatePending
   } = state;
-
   return (
     <SmallPlayer>
-      <ArtworkImage artwork={artwork} wrapperClass='small'/>
+      <ArtworkImage artwork={artwork} wrapperClass='small' />
       <Information className="small">
         <Track color={secondaryColor}>{track}</Track>
         <Artist color={tercaryColor}>{artist}</Artist>
         <Album color={tercaryColor}>{album}</Album>
-        <Progress progressColor={secondaryColor} emptyColor={tercaryColor} className="small" percent={elapsed / duration * 100}/>
+        <Progress progressColor={secondaryColor} emptyColor={tercaryColor} className="small" percent={duration ? (elapsed / duration) * 100 : 0} />
       </Information>
     </SmallPlayer>
-  )
-}
+  );
+};
 
-// Render function
 export const render = (state, dispatch) => {
-  const { size, horizontalPosition, verticalPosition, alwaysShow } = options;
-  const { app, playing, appleMusicError, songChange, primaryColor, secondaryColor, tercaryColor, artwork, song, updateAvailable, updatePending } = state;
-
-  // Determine widget visability
+  const { horizontalPosition, verticalPosition, alwaysShow } = options;
+  const { app, playing, songChange, primaryColor } = state;
   const showWidget = playing || (alwaysShow === 1 && app) || alwaysShow === 2;
-
-  // When song changes, prepare artwork
   if (songChange) {
-    prepareArtwork(dispatch, song);
+    prepareArtwork(dispatch, state.song);
   }
-
-  // Render
   return (
     <Wrapper show={showWidget} bg={primaryColor} horizontal={horizontalPosition} vertical={verticalPosition}>
-      <Small state={state} dispatch={dispatch}/>
+      <Small state={state} />
     </Wrapper>
-  )
+  );
 };
