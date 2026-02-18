c15398584cb0ef306bb7bd24fd71ee86ea5966fb
Dean Herbert <pe@ppy.sh>
2026-02-19 01:14:11 +0900

    Improve `fzf` command performance and visual style

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -28,6 +28,35 @@ export DOTNET_CLI_TELEMETRY_OPTOUT="true"
 export FZF_TMUX=1
 export FZF_TMUX_OPTS="-p 80%"
 
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow \
+    --exclude .git \
+    --exclude node_modules \
+    --exclude .npm \
+    --exclude .cache \
+    --exclude "*cache*" \
+    --exclude .Trash \
+    --exclude Applications \
+    --exclude Library/Caches \
+    --exclude Library/Logs \
+    --exclude Library/Mail \
+    --exclude Library/Safari \
+    --exclude Library/Containers \
+    --exclude vendor \
+    --exclude undodir \
+    --exclude __pycache__ \
+    --exclude .venv \
+    --exclude venv \
+    --exclude target \
+    --exclude Workspaces \
+    --exclude build \
+    --exclude dist \
+    --exclude "*.app"'
+
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+
+# Additional options for better UX
+set -gx FZF_DEFAULT_OPTS '--height 80% --layout=reverse --border --preview "head -100 {}"'
+
 alias ls='eza'
 
 if status is-interactive
